6c4e16e1cae8e107ffd726ab2da6739da8a33946
adrigm95 <adriangarciam95@gmail.com>
2022-08-18 18:44:54 +0200

    + Botón de partida rápida. + modal de nuevo mapa en editor
    + Botón de partida rápida. + modal de nuevo mapa en editor

---
+++ Hearts Of Ink/Assets/Scripts/Controller/MapEditor/CreateMapPanelController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateMapPanelController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject rightPanelController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void EnablePanel()
+    {
+        this.gameObject.SetActive(true);
+        rightPanelController.SetActive(false);
+    }
+
+    public void CancelCreation()
+    {
+        this.gameObject.SetActive(false);
+        rightPanelController.SetActive(true);
+    }
+
+    public void CreateMap()
+    {
+
+    }
+}
