0fc3d8e42fcdc14afa91c791bf72102e41a08af1
adrigm95 <adriangarciam95@gmail.com>
2023-01-24 22:36:38 +0100

    Finalizado rework gráfico de la pantalla de créditos

---
+++ Hearts Of Ink/Assets/Scripts/Controller/Generic/CreditsController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsController : MonoBehaviour
+{
+    public SceneChangeController sceneChangeController;
+    public float Speed;
+    public float EndPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newYPosition = transform.position.y + Speed * Time.deltaTime;
+
+        if (newYPosition > EndPosition)
+        {
+            Debug.Log("Changing scene, newYPosition: " + newYPosition);
+            sceneChangeController.ChangeScene(SceneChangeController.Scenes.MainMenu);
+        }
+        else
+        {
+            transform.position = new Vector3(transform.position.x, newYPosition, transform.position.z);
+        }
+    }
+}
