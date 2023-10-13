0617dd42c10bfdc2172919bd0ff476789a95ae79
leurzz <leur311@gmail.com>
2023-10-13 19:47:11 +0200

    Puntuación iniciada con algo
    🎂

---
+++ Hearts Of Ink/Assets/ScoreManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    private float width;
+    private RectTransform rectTransform;
+    private GameObject activePanel;
+
+    public float SizeClosed;
+    public float SizeOpen;
+    public GameObject scorePanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rectTransform = transform.GetComponent<RectTransform>();
+        width = rectTransform.sizeDelta.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void ActivatePanel(GameObject gameObject)
+    {
+        if (activePanel != null)
+        {
+            activePanel.SetActive(false);
+            activePanel.SetActive(true);
+        }
+        else if (activePanel == null)
+        {
+            activePanel.SetActive(true);
+            ToggleVisibility();
+        }
+    }
+    public void ToggleVisibility()
+    {
+        if (rectTransform.sizeDelta.y == SizeClosed)
+        {
+            Show();
+        }
+        else
+        {
+            Hide();
+        }
+    }
+    private void Show()
+    {
+        rectTransform.sizeDelta = new Vector2(width, SizeOpen);
+        scorePanel.SetActive(true);
+    }
+    private void Hide()
+    {
+        rectTransform.sizeDelta = new Vector2(width, SizeClosed);
+        scorePanel.SetActive(false);
+    }
+
+
+}
