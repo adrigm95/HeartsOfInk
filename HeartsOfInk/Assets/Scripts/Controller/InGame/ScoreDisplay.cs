41025b8b8bcfe18acbac92e2e8afef897a9ef6c2
leurzz <leur311@gmail.com>
2024-05-03 20:01:44 +0200

    Subida de colores en el score que antes no se ha subido putounity
    :D

--- HeartsOfInk/Assets/Scripts/Controller/InGame/ScoreDisplay.cs
+++ HeartsOfInk/Assets/Scripts/Controller/InGame/ScoreDisplay.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using Assets.Scripts.Utils;
+using NETCoreServer.Models;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,20 +9,32 @@ public class ScoreDisplay : MonoBehaviour
 {
     //GameObject[] cities = GameObject.FindGameObjectsWithTag("City");
     public TextMeshProUGUI[] scoreTexts = new TextMeshProUGUI[4]; // Asignar en el editor de unity
+    private float width;
+    private RectTransform rectTransform;
+    private GameObject activePanel;
+
+    public float SizeClosed;
+    public float SizeOpen;
+    public GameObject scorePanel;
     private int[] scoreTotal = new int[4]; // se asume que hay 4 facciones pero habría que ajustarlo para que las detecte automáticamente
     public FactionScore factionScore;
-
+    public GlobalLogicController globalLogicController;
     void Start()
     {
+    
+        rectTransform = transform.GetComponent<RectTransform>();
+        width = rectTransform.sizeDelta.x;
+
+        ActivatePanel(scorePanel);
         for (int i = 0; i < scoreTotal.Length; i++)
         {
             if (scoreTexts[i] != null)
             {
-                scoreTexts[i].text = "Facción " + (i+1) + ": " + scoreTotal[i];
+                scoreTexts[i].text = "Facción " + (i + 1) + ": " + scoreTotal[i];
             }
             else
             {
-                Debug.LogError("scoreTexts[" + (i+1) + "] nulo");
+                Debug.LogError("scoreTexts[" + (i + 1) + "] nulo");
             }
         }
     }
@@ -40,14 +54,51 @@ public class ScoreDisplay : MonoBehaviour
             if (troopController != null)
             {
                 int factionIndex = troopController.troopModel.Player.MapSocketId;
+                
                 scoreTotal[factionIndex] += troopController.troopModel.Units;
             }
         }
 
         for (int i = 0; i < scoreTotal.Length; i++)
         {
-            scoreTexts[i].text = "Facción " + (i+1) + ": " + scoreTotal[i];
+            Player player = globalLogicController.GetPlayer((byte)i);
+            Color color = ColorUtils.GetColorByString(player.Color);
+            scoreTexts[i].text = player.Name  + ": " + scoreTotal[i];
+            scoreTexts[i].GetComponent<TextMeshProUGUI>().color = color;
         }
     }
 
+    private void ActivatePanel(GameObject gameObject)
+    {
+        if (activePanel != null)
+        {
+            activePanel.SetActive(false);
+            activePanel.SetActive(true);
+        }
+        else if (activePanel == null)
+        {
+            ToggleVisibility();
+        }
+    }
+    public void ToggleVisibility()
+    {
+        if (rectTransform.sizeDelta.y == SizeClosed)
+        {
+            Show();
+        }
+        else
+        {
+            Hide();
+        }
+    }
+    private void Show()
+    {
+        rectTransform.sizeDelta = new Vector2(width, SizeOpen);
+        scorePanel.SetActive(true);
+    }
+    private void Hide()
+    {
+        rectTransform.sizeDelta = new Vector2(width, SizeClosed);
+        scorePanel.SetActive(false);
+    }
 }
--- HeartsOfInk/Assets/Scripts/Controller/ScoreManager.cs
+++ HeartsOfInk/Assets/Scripts/Controller/ScoreManager.cs
@@ -1,8 +1,8 @@
-﻿using Assets.Scripts.Data;
-using NETCoreServer.Models;
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Assets.Scripts.Data;
+using NETCoreServer.Models;
 using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
@@ -10,37 +10,23 @@ public class ScoreManager : MonoBehaviour
     private float width;
     private RectTransform rectTransform;
     private GameObject activePanel;
-    private StatisticsController statisticsControllerInstance;
 
     public float SizeClosed;
     public float SizeOpen;
     public GameObject scorePanel;
-    public TMPro.TextMeshProUGUI playerID;
-    public TMPro.TextMeshProUGUI[] playerTexts;
+
     // Start is called before the first frame update
     void Start()
     {
         rectTransform = transform.GetComponent<RectTransform>();
         width = rectTransform.sizeDelta.x;
-        statisticsControllerInstance = FindObjectOfType<StatisticsController>();
 
-        if (statisticsControllerInstance == null)
-        {
-            Debug.Log("StatisticsController not found.");
-            return;
-        }
-        else
-        {
-            ActivatePanel(scorePanel);
-            UpdateTexts();
-        }
+        ActivatePanel(scorePanel);
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Update() { }
+
     private void ActivatePanel(GameObject gameObject)
     {
         if (activePanel != null)
@@ -53,6 +39,7 @@ public class ScoreManager : MonoBehaviour
             ToggleVisibility();
         }
     }
+
     public void ToggleVisibility()
     {
         if (rectTransform.sizeDelta.y == SizeClosed)
@@ -64,55 +51,16 @@ public class ScoreManager : MonoBehaviour
             Hide();
         }
     }
+
     private void Show()
     {
         rectTransform.sizeDelta = new Vector2(width, SizeOpen);
         scorePanel.SetActive(true);
     }
+
     private void Hide()
     {
         rectTransform.sizeDelta = new Vector2(width, SizeClosed);
         scorePanel.SetActive(false);
     }
-    public bool IsAlly(Player factionId, Player factionId2)
-    {
-        FactionStatistics faction1 = statisticsControllerInstance.GetFaction(factionId);
-        FactionStatistics faction2 = statisticsControllerInstance.GetFaction(factionId2);
-        return faction1 != null && faction2 != null && faction1 == faction2;
-    }
-    public void SetPlayerID(Player factionId)
-    {
-        playerID.text = factionId.ToString();
-    }
-
-    public void UpdateTexts()
-    {
-        for (int i = 0; i < playerTexts.Length; i++)
-        {
-            if (playerTexts[i].text != null)
-            {
-                string[] splitText = playerTexts[i].text.Split(':');
-
-                if (splitText.Length >= 1)
-                {
-                    string playerText = splitText[0].Trim();
-                    FactionStatistics currentFaction = statisticsControllerInstance.GetFactionByName(playerText);
-
-                    if (currentFaction != null)
-                    {
-                        playerTexts[i].text = playerText + ": " + currentFaction.Player.ToString();
-                    }
-                    else
-                    {
-                        Debug.Log("Faction not found.");
-                    }
-                }
-                else
-                {
-                    Debug.LogError("Invalid player text format: " + playerTexts[i].text);
-                }
-            }
-        }
-    }
 }
-
