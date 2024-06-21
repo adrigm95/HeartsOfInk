7e94f616107187fc8cdff8f3f643b50f0221bc6d
Sara <sareq95@gmail.com>
2024-06-21 13:39:28 +0200

    Saltar la pantalla de aceptación de politicas cuando ya has aceptado las politicas

--- HeartsOfInk/Assets/Scripts/Controller/AcceptPolicy/AcceptTermsController.cs
+++ HeartsOfInk/Assets/Scripts/Controller/AcceptPolicy/AcceptTermsController.cs
@@ -2,6 +2,17 @@ using UnityEngine;
 using Assets.Scripts.DataAccess;
 public class AcceptTermsController : MonoBehaviour
 {
+    public SceneChangeController SceneChangeController;
+
+    public void Start()
+    {
+        string acceptTermsDate = AcceptTermsDAC.LoadAcceptTerms();
+
+        if (!string.IsNullOrWhiteSpace(acceptTermsDate))
+        {
+            SceneChangeController.DirectChangeScene(SceneChangeController.Scenes.MainMenu);
+        }
+    }
 
     public static void SaveTerms()
     {
--- HeartsOfInk/Assets/Scripts/DataAccess/AcceptTermsDAC.cs
+++ HeartsOfInk/Assets/Scripts/DataAccess/AcceptTermsDAC.cs
@@ -7,11 +7,25 @@ namespace Assets.Scripts.DataAccess
     public class AcceptTermsDAC
     {
         private const string FileName = "/acceptTerms.txt";
+
+        public static string LoadAcceptTerms()
+        {
+            try
+            {
+                return JsonCustomUtils<string>.ReadObjectFromFile(GetFile());
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
         public static void SaveAcceptTerms()
         {
             JsonCustomUtils<string>.SaveObjectIntoFile(DateTime.Now.Ticks.ToString(), GetFile());
             Debug.Log(GetFile());
         }
+
         private static string GetFile()
         {
             return Application.persistentDataPath + FileName;
