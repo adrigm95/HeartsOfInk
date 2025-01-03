7795036b1b86e211f4d1bbab4f7d87052d2ab4fc
Sara <sareq95@gmail.com>
2025-01-03 18:59:58 +0100

    Método provisional de encriptado

---
+++ HeartsOfInk/Assets/Scripts/Logic/SecurityLogic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+using UnityEngine;
+
+public class SecurityLogic
+{
+    System.Random random = new System.Random();
+
+    public string CreatePassword(string newPassword)
+    {
+        return HashPassword(newPassword, string.Empty);
+    }
+
+    private string HashPassword(string password, string salt)
+    {
+        Debug.LogWarning("Temporal implementation for password");
+        return Convert.ToBase64String(Encoding.UTF8.GetBytes(password + "TfR562"));
+    }
+}
